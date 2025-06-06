f3b72a9b8912c197482cab696082df56d555d8dd
KingSagnik <sdey03575@gmail.com>
2025-06-07 01:30:55 +0530

    Add contract interaction script for CertificateIssuer

--- contracts/contracts/Lock.sol
+++ contracts/contracts/Lock.sol
@@ -1,34 +1,21 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.28;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract CertificateIssuer is ERC721URIStorage, Ownable {
+    uint256 public nextCertificateId;
 
-    event Withdrawal(uint amount, uint when);
+    event CertificateIssued(address indexed to, uint256 indexed certificateId, string uri);
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+    constructor() ERC721("Blockchain Certificate", "CERT") Ownable(msg.sender) {}
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function issueCertificate(address to, string memory uri) public onlyOwner {
+        uint256 certificateId = nextCertificateId;
+        _safeMint(to, certificateId);
+        _setTokenURI(certificateId, uri);
+        emit CertificateIssued(to, certificateId, uri);
+        nextCertificateId++;
     }
 }
--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox"); // This includes hardhat-ethers, chai, etc.
+// or, if you want only ethers:
+require("@nomicfoundation/hardhat-ethers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    core: {
+      url: "https://rpc.test2.btcs.networkg", // Example: CORE mainnet RPC
+      chainId: 1114, // Example: CORE mainnet chain ID
+      accounts: ["add611716697fa0d59513b2dfd6ec78f58c4eec551cec6102f89faba044f1c36"] // Replace with your deployer wallet's private key
+    }
+  }
 };
